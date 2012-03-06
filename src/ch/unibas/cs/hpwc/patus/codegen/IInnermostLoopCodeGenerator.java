83ca8f51f0aa2905b28bbdc30d817d701524baab
mmchristen@yahoo.com <mmchristen@yahoo.com@a01b5346-091d-b3ab-1409-f2a5165db27e>
2012-03-06 16:21:44 +0000

    inline assembly code generation: cg integration

--- src/ch/unibas/cs/hpwc/patus/codegen/IInnermostLoopCodeGenerator.java
+++ src/ch/unibas/cs/hpwc/patus/codegen/IInnermostLoopCodeGenerator.java
@@ -1,8 +1,21 @@
 package ch.unibas.cs.hpwc.patus.codegen;
 
+import cetus.hir.Traversable;
+import ch.unibas.cs.hpwc.patus.ast.StatementListBundle;
+import ch.unibas.cs.hpwc.patus.codegen.options.CodeGeneratorRuntimeOptions;
+
 
 public interface IInnermostLoopCodeGenerator extends ICodeGenerator
 {
+	/**
+	 * Generates code for an innermost loop containing a stencil call.
+	 * @param sdit The subdomain iterator containing the innermost loop with a stencil computation
+	 * 	for which to generate the code 
+	 * @param options Code generation-time options
+	 */
+	@Override
+	public StatementListBundle generate (Traversable sdit, CodeGeneratorRuntimeOptions options);
+	
 	/**
 	 * Tells whether for the instance of the inner most loop code generator the assembly
 	 * section in the architecture description is required.
